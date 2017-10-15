5d26fd313db62a37805043dafff546377c839408
Eric Griffith <EGriffith92@gmail.com>
2017-10-14 20:31:26 -0400

    Fix UID typo.

--- docker-run-plex.sh
+++ docker-run-plex.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-docker run -d --name plex --network=host -e TZ=America/New_York -e PLEX_UID=994 -e PLEX_GID=1004 -v plex_config:/config -v plex_transcode:/transcode -v plex_media:/data:ro --restart=unless-stopped plexinc/pms-docker:plexpass
+docker run -d --name plex --network=host -e TZ=America/New_York -e PLEX_UID=992 -e PLEX_GID=1004 -v plex_config:/config -v plex_transcode:/transcode -v plex_media:/data:ro --restart=unless-stopped plexinc/pms-docker:plexpass
